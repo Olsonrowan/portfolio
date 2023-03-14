ad07f905fc7d1e8c314f86c52e09eebab4ea62f9
Rowan Olson <Olsonrowan@gmail.com>
2023-03-13 22:18:31 -0600

    Footer changes, took out unnecessary links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,23 +1,10 @@
-import React from 'react';
+import React from "react";
+import "../../styles/styles.css";
 
 function Footer() {
   return (
     <footer>
-      <p>Copyright 2023</p>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/projects">Projects</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-      </ul>
+      <p className="footerInfo">Copyright 2023</p>
     </footer>
   );
 }
