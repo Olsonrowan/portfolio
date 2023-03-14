8b16653ce9e498b4d12b1750b94504b903ec6b94
Rowan Olson <Olsonrowan@gmail.com>
2023-03-13 22:19:15 -0600

    added logo and general layout of header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
+import "../../styles/styles.css";
+import logo from "../../assets/logo.png";
 
 function Header() {
   return (
     <header>
-      <nav>
-        <ul>
-          <li>
-            Home
-          </li>
-          <li>
-            About
-          </li>
-          <li>
-            Projects
-          </li>
-          <li>
-           Contact
-          </li>
-        </ul>
-      </nav>
+      <div className="logoContainer">
+        <img className="logo" alt="My Logo" src={logo}></img>
+      </div>
+      <ul className="links">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/projects">Projects</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
     </header>
   );
 }
